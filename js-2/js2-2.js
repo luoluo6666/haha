b51c1b0f03a8c7d40d774d0fe2bbc275b4363a5b
luoluo6666 <32211557+luoluo6666@users.noreply.github.com>
2017-12-05 17:13:41 +0800

    Add files via upload

--- js-2/js2-2.js
+++ js-2/js2-2.js
@@ -1,42 +1,33 @@
 /**
  * Created by Administrator on 2017/11/21.
  */
-
-function get(){
-     var Num = document.getElementById("import").value;
-    var mask = document.getElementById("mask");
-    var login = document.getElementById("login");
-    var close_login=document.getElementById("close_login");
-
-    if(Num>=4&&Num<=18) {
-            location.href='js-3-Check the status.html';
-    }
-    else{                          
-        mask.style.display="block";       //弹出模态框，提示请输入正确的玩家数量
-        login.style.display="block";
-        close_login.onclick=function(){    //点击确定或取消，模态框隐藏
-            mask.style.display="none";
-            login.style.display="none";
-        };
-    }
-}
+var flag=false;
 
 // 分出杀手和平民各有多少人
 function allot() {
     var Num = document.getElementById("import").value;
     var b = document.getElementById("killer");
     var c = document.getElementById("people");
-    if(Num>=4&&Num<=18){
-        document.getElementById("killer").innerHTML= Math.floor(Num/4);
-        b.innerHTML = Math.floor(Num/4);
-        c.innerHTML = Num-b.innerHTML;
-    }
-    else {
+    var mask = document.getElementById("mask");
+    var login = document.getElementById("login");
+    var close_login = document.getElementById("close_login");
+
+    if (Num >= 4 && Num <= 18) {
+        document.getElementById("killer").innerHTML = Math.floor(Num / 4);
+        b.innerHTML = Math.floor(Num / 4);
+        c.innerHTML = Num - b.innerHTML;
+        
+    } else {
         b.innerHTML = "";
         c.innerHTML = "";
+        mask.style.display = "block";       //弹出模态框，提示请输入正确的玩家数量
+        login.style.display = "block";
+        close_login.onclick = function () {    //点击确定或取消，模态框隐藏
+            mask.style.display = "none";
+            login.style.display = "none";
+        }
     }
 }
-
 // 随机分配平民杀手
 var num=document.getElementById("num");
 num.onclick=function () {
@@ -44,25 +35,39 @@ num.onclick=function () {
     var KillerNum= Math.floor(Num/4);//杀手数量
     var TotalArr = [];
     var k = " ";
-    for(var n=0;n<KillerNum;n++){
-        TotalArr[n]="杀手"
-    }
-    for(n=KillerNum;n<Num;n++){
-        TotalArr[n]="平民"
-    }
-    TotalArr.sort(function (){return 0.5 - Math.random() });//数组乱序
+    if(Num >= 4 && Num <= 18){
+        for(var n=0;n<KillerNum;n++){
+            TotalArr[n]="杀手"
+        }
+        for(n=KillerNum;n<Num;n++){
+            TotalArr[n]="平民"
+        }
+        TotalArr.sort(function (){return 0.5 - Math.random() });//数组乱序
 
-    for (var i = 0; i < Num; i++) {
-        var m = i+1;
-        k += m +'号'+TotalArr[i]+"<br>";
-    }
-    document.getElementById("per").innerHTML = k;//输出杀手平民分配情况
+        for (var i = 0; i < Num; i++) {
+            var m = i+1;
+            k += m +'号'+TotalArr[i]+"<br>";
+        }
+        document.getElementById("per").innerHTML = k;//输出杀手平民分配情况
 
-    s = JSON.stringify(TotalArr);  //数组转换成字符串
-    console.log(TotalArr);
-     //存入
-    sessionStorage.TotalArr = s;
-   
+        s = JSON.stringify(TotalArr);  //数组转换成字符串
+        console.log(TotalArr);
+        //存入
+        sessionStorage.TotalArr = s;
+    }
+    else {
+        alert("请输入正确的人数")
+    }
+
+    flag=true;
+};
+document.getElementById('go').onclick=function () {
+    if(flag){
+        location.href = 'js-3-Check the status.html';
+    }
+    else {
+        alert("请点击设置")
+    }
 };
 
 
